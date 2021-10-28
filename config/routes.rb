00226ff54eab06c18269859bf2a61cce1061e622
Jessica Jones <daddehjones@gmail.com>
2021-10-27 20:54:44 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index, :show]
       end
     end
-  end 
+  end
 end
