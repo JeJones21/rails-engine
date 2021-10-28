b97fcb9cc2967f39ff7b0d12265d6bb6ebb6ae7f
Jessica Jones <daddehjones@gmail.com>
2021-10-28 10:42:06 -0400

    Create update action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -23,13 +23,18 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(item), status: 201
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    render json: ItemSerializer.new(item)
+  end
+
   def destroy
     item = Item.find(params[:id])
     item.destroy
   end
 
   private
-
 
   def item_params
     params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
