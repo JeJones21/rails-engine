1afb47419d57ac2e4bea7b98f66c9d6407199bfe
Jessica Jones <daddehjones@gmail.com>
2021-10-28 20:04:01 -0400

    Add revenue methods.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #option 1 of section 2
+
   namespace :api do
     namespace :v1 do
+      get '/merchants/find', to: 'merchants#find'
+      get '/items/find_all_items', to: 'items#find_all_items'
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index, :show]
       end
@@ -10,6 +15,7 @@ Rails.application.routes.draw do
       end
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: 'merchants/items'
+      end
     end
   end
 end
